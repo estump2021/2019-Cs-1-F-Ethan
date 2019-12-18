c255740af9d6b6837e80e6996bc3861eca40b4ec
estump2021 <estump21@nscds.org>
2019-12-18 18:40:06 +0000

    changes

---
+++ Whack-A-Mole/finalproject.js
@@ -0,0 +1,21 @@
+
+var random = Math.random();
+var score = 0 + a;
+var a = 
+
+$('.mole').click(
+    function(){
+        $(this).slideUp(); 
+    }
+);
+$('#start').click(
+    function(){
+        $('.mole').slideUp(function() {
+        ('.mole').slideDown(500);
+  })}
+);
+
+
+
+
+
