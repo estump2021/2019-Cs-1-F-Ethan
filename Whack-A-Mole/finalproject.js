c72d5557c4c204b15d856ed2f0f57f42b9472cbd
estump2021 <estump21@nscds.org>
2020-02-04 19:17:08 +0000

    Math random work

--- Whack-A-Mole/finalproject.js
+++ Whack-A-Mole/finalproject.js
@@ -1,27 +1,34 @@
 
 var score = 0 
+var randMole = Math.floor((Math.random() * 8) + 1  );
 
 $('.mole').click(
     function(){
         $(this).slideUp();
         score++;
-        $("#score").text(score)
-        $()
-    
+        $("#score").text(score);
+        
     }
     
 );
-$('#start').click(
-    function () {
-        $('.mole').slideUp(function() {
-        ('.mole').slideDown(500);
-  })}
-);
-$('#slideUp').click(
-        function() {
-        $('.mole').slideDown();
-  }
+$('#start').click(function() {
+        $('.mole').slideUp();
+    }
 );
 
+
+setTimeout(function showMole() {
+    //pick a number between 1 and 8
+    var randMole = Math.floor((Math.random() * 8) + 1  );
+    $(".mole"+randMole).slideDown();
+
+    //setTimeout(alertFunc, 3000);
+}, 3000) 
+ 
+
+
+
+
+console.log(randMole);
 
 
